e5f192ca2e7338ba0d03500fe8ac9c2931b0c561
Dasunika0216 <dasunikamd.22@uom.lk>
2025-07-04 21:27:42 +0530

    Enhance HomeScreen component by adding image display and horizontal FlatList for chat faces

--- app/Pages/HomeScreen.js
+++ app/Pages/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, FlatList } from 'react-native';
 import ChatFaceData from '../Services/ChatFaceData';
 
 export default function HomeScreen() {
@@ -16,6 +16,20 @@ export default function HomeScreen() {
     <View style={{alignItems:'center', paddingTop:90}}>
       <Text style={[{color:selectedChatFaceData.primary}, {fontSize:30}]}>Hello</Text>
       <Text style={[{color:selectedChatFaceData.primary}, {fontSize:30, fontWeight:'bold'}]}>I am {selectedChatFaceData.name}</Text>
+      <Image source={{uri:selectedChatFaceData.image}} style={{width:150, height:150, marginTop:20}}/>
+      <Text style={{marginTop:30, fontSize:25}}>How can I help you?</Text>
+
+      <View>
+        <FlatList
+            data={chatFaceData}
+            horizontal={true}
+            renderItem={({item}) => selectedChatFaceData.id != item.id && (
+                <View style={{margin:15}}>
+                    <Image source={{uri:item.image}} style={{width:40, height:40}} />
+                </View>
+            )}
+        />
+      </View>
     </View>
   )
 }
